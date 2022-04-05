089317b8ef79e3cccb4c63c1c3cea838a003a2cc
scottfreiberg6 <scott.freiberg6@gmail.com>
2022-04-05 17:45:18 -0400

    user/chain assignment

---
+++ fundamentals/freiberg_scott_user.py
@@ -0,0 +1,26 @@
+class User:
+    def __init__(self,name, email,account_balance):
+        self.name=name
+        self.email=email
+        self.account_balance = account_balance
+    
+    def make_deposit(self, amount):
+        self.account_balance += amount
+        return self
+    
+    def make_withdrawal(self, amount):
+        self.account_balance -= amount
+        return self
+
+michael = User("michael", "m@d.com", 0)
+anna = User("anna", "a@d.com", 0)
+scott = User("scott", "s@d.com", 0)
+
+scott.make_deposit(100).make_deposit(150).make_deposit(150).make_withdrawal(50)
+print(scott.account_balance)
+
+michael.make_deposit(150).make_deposit(150).make_withdrawal(50).make_withdrawal(100)
+print(michael.account_balance)
+
+anna.make_deposit(400).make_withdrawal(50).make_withdrawal(50).make_withdrawal(50)
+print(anna.account_balance)
